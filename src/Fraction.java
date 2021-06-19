51e75c82b363442d6b287454aa3a90c3ff64f90f
mregazzi <mregazzi@tiscali.it>
2021-06-19 21:56:48 +0200

    Now we can sum fractions

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Fraction {
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        //REFACTOR A fraction can be constructed already to lowest terms
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction(int numerator) {
+        //REFACTOR Should use the basic constructor
+        this.numerator = numerator;
+        this.denominator = 1;
+    }
+
+    public Fraction add(Fraction addendum) {
+        return new Fraction((this.numerator * addendum.denominator) +
+                (addendum.numerator * this.denominator),
+                this.denominator * addendum.denominator).toLowestTerms();
+    }
+
+    private Fraction toLowestTerms() {
+        int MCD = this.calcMCDWithEuclide(numerator, denominator);
+        return new Fraction(numerator / MCD, denominator / MCD);
+    }
+
+    private int calcMCDWithEuclide(int m, int n) {
+        int i = Math.min(m, n);
+        int j = Math.max(m, n);
+
+        int tmpInt;
+        while ((tmpInt = i % j) != 0) {
+            i = j;
+            j = tmpInt;
+        }
+        return j;
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
 }
--- test/FractionTest.java
+++ test/FractionTest.java
@@ -1,2 +1,81 @@
-package PACKAGE_NAME;public class FractionTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+//REFACTOR Test names are not good, they should express intention, not the test data
+public class FractionTest {
+    @Test
+    public void sameDenominator() {
+        Fraction firstAddendum = new Fraction(1, 4);
+        Fraction secondAddendum = new Fraction(2, 4);
+
+        Assertions.assertEquals("3/4", firstAddendum.add(secondAddendum).toString());
+
+        firstAddendum = new Fraction(1, 5);
+        secondAddendum = new Fraction(2, 5);
+
+        Assertions.assertEquals("3/5", firstAddendum.add(secondAddendum).toString());
+    }
+
+    @Test
+    public void sameDenominatorTheSumIsTheWhole() {
+        Fraction firstAddendum = new Fraction(1, 2);
+        Fraction secondAddendum = new Fraction(1, 2);
+
+        Assertions.assertEquals("1/1", firstAddendum.add(secondAddendum).toString());
+    }
+
+    @Test
+    public void anIntPlusAFractionsameDenominator() {
+        Fraction firstAddendum = new Fraction(1);
+        Fraction secondAddendum = new Fraction(1, 1);
+
+        Assertions.assertEquals("2/1", firstAddendum.add(secondAddendum).toString());
+    }
+
+    @Test
+    public void anIntPlusAFractionDifferentDenominator() {
+        Fraction firstAddendum = new Fraction(1);
+        Fraction secondAddendum = new Fraction(1, 2);
+
+        Assertions.assertEquals("3/2", firstAddendum.add(secondAddendum).toString());
+    }
+
+    @Test
+    public void differentDenominatorSumIsMoreThanOne() {
+        Fraction firstAddendum = new Fraction(1, 2);
+        Fraction secondAddendum = new Fraction(2, 3);
+
+        Assertions.assertEquals("7/6", firstAddendum.add(secondAddendum).toString());
+    }
+
+    @Test
+    public void TwoOverFourPlusFouwOverEightShouldBeEqualsToOneOverOne() {
+        Fraction firstAddendum = new Fraction(2, 4);
+        Fraction secondAddendum = new Fraction(4, 8);
+
+        Assertions.assertEquals("1/1", firstAddendum.add(secondAddendum).toString());
+    }
+
+    @Test
+    public void FourOverSixShouldBeTwoOverThree() {
+        Fraction firstAddendum = new Fraction(1, 6);
+        Fraction secondAddendum = new Fraction(3, 6);
+
+        Assertions.assertEquals("2/3", firstAddendum.add(secondAddendum).toString());
+    }
+
+    @Test
+    public void zeroIsZero() {
+        Fraction firstAddendum = new Fraction(0);
+        Fraction secondAddendum = new Fraction(1);
+
+        Assertions.assertEquals("1/1", firstAddendum.add(secondAddendum).toString());
+    }
+
+    @Test
+    public void StrangeOne() {
+        Fraction firstAddendum = new Fraction(7, 3);
+        Fraction secondAddendum = new Fraction(4, 5);
+
+        Assertions.assertEquals("47/15", firstAddendum.add(secondAddendum).toString());
+    }
 }
